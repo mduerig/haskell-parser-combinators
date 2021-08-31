f074851de121e2bb378cbbf20e0d22b2e16820be
Michael Dürig <michid@gmail.com>
2021-08-31 23:18:27 +0200

    Introduce ParseResult

--- src/Hpc.hs
+++ src/Hpc.hs
@@ -4,9 +4,14 @@ module Hpc where
 
 import Control.Applicative (Applicative, Alternative, empty, (<|>))
 import Control.Monad ((>=>))
+import GHC.Base (Semigroup)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data ParseResult a =
+    ParseResult (Either ParseError a) String
+    deriving Show
 
 data ParseError
     = PrematureEOF
@@ -14,47 +19,56 @@ data ParseError
     | UnexpectedInput
     deriving Show
 
+instance Functor ParseResult where
+    fmap f (ParseResult r s) = ParseResult (fmap f r) s
+
+instance Semigroup (ParseResult a) where
+  (ParseResult (Left _) _) <> r = r
+  r <> _ = r
+
 newtype Parser a = Parser
-    { parse :: String -> Either ParseError (String, a)
+    { parse :: String -> ParseResult a
     }
 
 instance Functor Parser where
-    fmap f p = Parser $ (fmap . fmap) f . parse p
+    fmap f p = Parser $ fmap f . parse p
 
 instance Applicative Parser where
   pure = success
   pf <*> pa = Parser $ \s ->
-      case parse pf s of
-          Right (s', f) -> parse (f <$> pa) s'
-          Left e -> Left e
+    let
+        ParseResult rf s'  = parse pf s
+        ParseResult ra s'' = parse pa s'
+    in
+        ParseResult (rf <*> ra) s''
 
 instance Monad Parser where
   pa >>= f = Parser $ \s ->
-      case parse pa s of
-          Right (s', a) -> parse (f a) s'
-          Left e -> Left e
+    case parse pa s of
+        ParseResult (Right x) s' -> parse (f x) s'
+        ParseResult (Left  e) s' -> ParseResult (Left e) s'
 
 instance Alternative Parser where
   empty = failure UnexpectedInput
   p1 <|> p2 = Parser $ parse p1 <> parse p2
 
 success :: a -> Parser a
-success x = Parser $ \s -> Right (s, x)
+success x = Parser $ \s -> ParseResult (Right x) s
 
 failure :: ParseError -> Parser a
-failure e = Parser $ \s -> Left e
+failure e = Parser $ \s -> ParseResult (Left e) s
 
 eof :: Parser ()
 eof = Parser $
     \case
-        "" -> Right ("", ())
-        _  -> Left ExpectedEOF
+        "" -> ParseResult (Right ()) ""
+        _  -> ParseResult (Left ExpectedEOF) ""
 
 anyChar :: Parser Char
 anyChar = Parser $
     \case
-        c:cs -> Right (cs, c)
-        _    -> Left PrematureEOF
+        c:cs -> ParseResult (Right c) cs
+        ""   -> ParseResult (Left PrematureEOF) ""
 
 char :: (Char -> Bool) -> Parser Char
 char predicate = do
